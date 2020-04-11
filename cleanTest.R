96dcabefdc16c194a6ce8e9c50531ab3aa569dd6
b9030342 <b9030342@my.shu.ac.uk>
2020-04-11 16:29:20 +0100

    2nd commit

--- cleanTest.R
+++ cleanTest.R
@@ -17,7 +17,7 @@ population <- read_csv("~/Dropbox/ADM Group Assessment/The Development/Structed 
 childcare <- read_excel("~/Dropbox/ADM Group Assessment/The Development/Education_Childcare_dataset_as_at_31_March_2018_new (version 1).xlsx",sheet = "Childcare_providers", col_types = c("text","text", "text", "text", "date", "text","text", "text", "text", "text", "text","text", "text", "text", "text", "text","text", "text", "text", "text", "text","numeric", "text", "text", "text","text", "text", "text", "text", "text","text", "text", "text", "text", "text","text", "text", "text", "text"))
 childcare2 <- read_csv("~/Dropbox/ADM Group Assessment/The Development/Structed Data/facilities.csv")
 housing <- read_csv("~/Dropbox/ADM Group Assessment/The Development/Structed Data/housing.csv")
-#
+
 ###########################################################################
 ###########################################################################
 
